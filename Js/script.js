2ea49874747ff61d954f16249d856517dc53bcd3
jlouifils <johnnylouifils1919@gmail.com>
2018-12-29 17:48:37 -0500

    edit 2

--- Js/script.js
+++ Js/script.js
@@ -1,9 +1,73 @@
 // create a spaceBar using HTML markup
 
 //give an form element a varible
-const spaceBar = s('<form action ="#" method="get"> </form>');
+const spaceBar = $('<form action ="#" method="get"> </form>');
 // i attach the spaceBar to two types of input: search and submit
 spaceBar.append('<input type="search" id="search-input" class="search-input" placeholder="Search...">',
-'<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">')
+'<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">');
 // attach $spaceBar inside the <div class="search-container">
 $('search-container').append(spaceBar);
+
+$.ajax({
+  url: 'https://randomuser.me/api/?results=12',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+
+    const info = data.results;
+    const gallery = '';
+    const modalBox = '';
+
+    $.each(info, function (index, employee) {
+        const name = employee.name.first + '' + employee.name.last;
+        const email = employee.email;
+        const city = employee.location.city;
+        const number = employee.phone;
+        const address = employee.location.street + '' + location + '' + employee.location.postcode;
+        const birthday = employee.dob.date;
+
+        modalBox += '<div class="modal-container">';
+        modalBox += '<div class="modal" data-index="'+index+'">';
+        modalBox += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
+        modalBox += '<div class="modal-info-container"><img class="modal-img" src= "' + picture + '" alt="profile picture"><h3 id="name" class="modal-name cap">' + name + '</h3><p class="modal-text">' + email + '</p><p class="modal-text cap">' + location + '</p>';
+        modalBox += '<hr>';
+        modalBox += '<p class="modal-text">' + number + '</p><p class="modal-text">' + address + '</p><p class="modal-text">' + birthday + '</p>';
+        modalBox += '</div></div>';
+
+
+        gallery += '<div class="card" data-index="'+index+'">';
+        gallery += '<div class="card-img-container">';
+        gallery += '<img class="card-img" src= "' + picture + '" alt="profile picture"></div>';
+        gallery += '<div class="card-info-container"><h3 id="name" class="card-name cap">' + name + '</h3>';
+        gallery += '<p class="card-text">' + email + '</p><p class="card-text cap">' + location + '</p>';
+        gallery += '</div></div>';
+
+        });
+
+      const card = $('.card');
+      const modal = $('.modal');
+      const modalContain = $('.modal-contanier')
+      const indexInfo = $(this).data('index');
+
+      $(document).ready( function () {
+        card.on('click', function() {
+          modalContain,modal.each( function(index){
+              if( $(this).data('index') === indexInfo) {
+                $(this).addClass('active');
+              } else {
+                $(this).removeClass('active');
+              }
+          });
+
+        });
+        const modalButton = $('#modal-close-btn');
+
+        modal,modalContain,modalButton.on('click', function() {
+          modal,modalContain.removeClass('active');
+        });
+      })
+      $('#gallery').append(gallery);
+      $('body').append(modalBox);
+    }
+
+})
