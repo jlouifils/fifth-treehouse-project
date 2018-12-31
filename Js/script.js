8b4451727b63308d2a5c320fe33eaf7cb1cd04ca
jlouifils <johnnylouifils1919@gmail.com>
2018-12-30 19:21:28 -0500

    Update script.js

--- Js/script.js
+++ Js/script.js
@@ -19,28 +19,28 @@ $.ajax({
     let modalBox = '';
 
     $.each(info, function (index, employee) {
-        const name = employee.name.first +  ''  +  employee.name.last;
+        const name = employee.name.first + " " + employee.name.last;
         const email = employee.email;
         const picture = employee.picture.large;
         const city = employee.location.city;
         const number = employee.phone;
-        const address = employee.location.street + '' + city + '' + employee.location.postcode;
+        const address = employee.location.street + " " + city + " " + employee.location.postcode;
         const birthday = employee.dob.date;
 
         modalBox += '<div class="modal-container">';
         modalBox += '<div class="modal" data-index="'+index+'">';
         modalBox += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
-        modalBox += '<div class="modal-info-container"><img class="modal-img" src= "' + picture + '" alt="profile picture"><h3 id="name" class="modal-name cap">"' + name + '"</h3><p class="modal-text">"' + email + '"</p><p class="modal-text cap">"' + city + '"</p>';
+        modalBox += '<div class="modal-info-container"><img class="modal-img" src= ' + picture + ' alt="profile picture"><h3 id="name" class="modal-name cap">' + name + '</h3><p class="modal-text">' + email + '</p><p class="modal-text cap">' + city + '</p>';
         modalBox += '<hr>';
-        modalBox += '<p class="modal-text">"' + number + '"</p><p class="modal-text">"' + address + '"</p><p class="modal-text">"' + birthday + '"</p>';
+        modalBox += '<p class="modal-text">' + number + '</p><p class="modal-text">' + address + '</p><p class="modal-text">' + birthday + '</p>';
         modalBox += '</div></div>';
 
 
         gallery += '<div class="card" data-index="'+index+'">';
         gallery += '<div class="card-img-container">';
-        gallery += '<img class="card-img" src= "' + picture + '" alt="profile picture"></div>';
-        gallery += '<div class="card-info-container"><h3 id="name" class="card-name cap">"' + name + '"</h3>';
-        gallery += '<p class="card-text">"' + email + '"</p><p class="card-text cap">"' + city + '"</p>';
+        gallery += '<img class="card-img" src= ' + picture + ' alt="profile picture"></div>';
+        gallery += '<div class="card-info-container"><h3 id="name" class="card-name cap">' + name + '</h3>';
+        gallery += '<p class="card-text">' + email + '</p><p class="card-text cap">' + city + '</p>';
         gallery += '</div></div>';
 
         });
