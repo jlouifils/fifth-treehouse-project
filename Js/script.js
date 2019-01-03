b59332db131dd09078e5caf3e53b27eb8d72c833
jlouifils <johnnylouifils1919@gmail.com>
2019-01-03 10:55:11 -0500

    update script.js

--- Js/script.js
+++ Js/script.js
@@ -6,7 +6,7 @@ const spaceBar = $('<form action ="#" method="get"> </form>');
 spaceBar.append('<input type="search" id="search-input" class="search-input" placeholder="Search...">',
 '<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">');
 // attach $spaceBar inside the <div class="search-container">
-$('search-container').append(spaceBar);
+$('.search-container').append(spaceBar);
 
 $.ajax({
   url: 'https://randomuser.me/api/?results=12',
@@ -27,12 +27,13 @@ $.ajax({
         const address = employee.location.street + " " + city + " " + employee.location.postcode;
         const birthday = employee.dob.date;
 
+         modalBox += '<div class="modal-container">'
          modalBox += '<div class="modal" data-index="'+index+'">';
          modalBox += '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
          modalBox += '<div class="modal-info-container"><img class="modal-img" src= ' + picture + ' alt="profile picture"><h3 id="name" class="modal-name cap">' + name + '</h3><p class="modal-text">' + email + '</p><p class="modal-text cap">' + city + '</p>';
          modalBox += '<hr>';
          modalBox += '<p class="modal-text">' + number + '</p><p class="modal-text">' + address + '</p><p class="modal-text">' + birthday + '</p>';
-         modalBox += '</div>';
+         modalBox += '</div></div>';
 
 
         gallery += '<div class="card" data-index="'+index+'">';
@@ -44,10 +45,11 @@ $.ajax({
 
         })
 
-      $('#gallery').append(gallery);
-      $('#gallery').append(modalBox);
+      $('div#gallery').append(gallery);
+      $('div.modal-container').append(modalBox);
 
 let indexInfo = $(this).data('index')
+
       $('.card').on('click',function() {
         console.log('card clicked');
         $(".modal", $(".modal-container")).each( function(index) {
@@ -66,4 +68,3 @@ let indexInfo = $(this).data('index')
 
 
 })
-const indexInfo = $(this).data('index');
